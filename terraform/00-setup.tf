49af75153a9ee738b3978054171c37f4bf7873c4
Sam Pearson <sam@sgp.me.uk>
2019-04-09 16:18:32 +0100

    Terraform: add terraform config for S3 bucket and user policy
    Also adds support in `script/build` to run this and `script/bootstrap`
    to check for Terraform when running a build.

---
+++ terraform/00-setup.tf
@@ -0,0 +1,28 @@
+terraform {
+  backend "s3" {}
+}
+
+variable "aws_region" {
+  description = "AWS region to hold the resources"
+  default     = "eu-west-1"
+}
+
+variable "wiki_user" {
+  description = "IAM username for accessing AWS resources"
+  default     = "wiki"
+}
+
+variable "bucket_domain_name" {
+  description = "Suffix for the name of the bucket for the Uploads. Will be prefixed with ms-wr-."
+  default     = "wiki-mysociety-org"
+}
+
+variable "expire_version_days" {
+  description = "Number of days before versions expire from the bucket"
+  default     = 28
+}
+
+provider "aws" {
+  region  = "${var.aws_region}"
+  version = "~> 2.1"
+}
---
+++ terraform/01-s3.tf
@@ -0,0 +1,52 @@
+resource "aws_s3_bucket" "wiki_resources" {
+  bucket = "ms-wr-${var.bucket_domain_name}"
+  versioning {
+    enabled = true
+  }
+  lifecycle_rule {
+    id      = "version_expiry"
+    enabled = true
+    noncurrent_version_expiration {
+      days = "${var.expire_version_days}"
+    }
+  }
+  lifecycle = {
+    prevent_destroy = true
+  }
+  tags {
+    Name      = "Wiki Resources for ${var.bucket_domain_name}"
+    Terraform = "true"
+  }
+}
+
+resource "aws_iam_user_policy" "wiki_resources_user_policy" {
+  name       = "${var.bucket_domain_name}-user-policy"
+  user       = "${var.wiki_user}"
+  depends_on = ["aws_s3_bucket.wiki_resources"]
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowAllS3ActionsFor${var.wiki_user}",
+            "Effect": "Allow",
+            "Action": "s3:*",
+            "Resource": [
+                "arn:aws:s3:::ms-wr-${var.bucket_domain_name}/*"
+            ]
+        },
+        {
+            "Sid": "AllowReadingFor${var.wiki_user}",
+            "Effect": "Allow",
+            "Action": [
+                "s3:Get*",
+                "s3:List*"
+            ],
+            "Resource": [
+                "arn:aws:s3:::ms-wr-${var.bucket_domain_name}"
+            ]
+        }
+    ]
+}
+EOF
+}
